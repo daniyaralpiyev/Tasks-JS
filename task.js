b0162cfa1225becb1834ca61960160c64b66d738
Daniyar <piranha0590@gmail.com>
2025-06-05 00:04:22 +0500

    added-task-lists

---
+++ task.js
@@ -0,0 +1,5 @@
+let inventory = [
+    {id: 1, name: 'Apple', price: 0.50, quantity: 200},
+    {id: 2, name: 'Banana', price: 0.30, quantity: 150},
+    {id: 3, name: 'Cherry', price: 0.90, quantity: 50}
+]
