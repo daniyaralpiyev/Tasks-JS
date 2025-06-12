eddb3d40c968764d0274bea453a4241971c98fda
Daniyar <piranha0590@gmail.com>
2025-06-12 09:16:36 +0500

    added api data

---
+++ api-data.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const url = 'https://icherniakov.ru/yt-course/account/test_accounts'
+
+// Получаем данные от сервера через fetch
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+// Получаем данные от сервера через async/await
+const getData = async () => {
+    try {
+        const response = await fetch(url)
+        const jsonData = await response.json();
+        console.log(jsonData);
+    } catch (error) {
+        console.error(error);
+    }
+}
+getData()
+
+// Получаем данные от сервера через axion
+const getDataAxion = async () => {
+    try {
+        const response = await axios.get(url)
+        console.log('Данные: ', response.data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+getDataAxion()
+
+axios.get(url)
+    .then(response => console.log(response.data));
